c6748c942aa1eabd9cfe7af7ec838dc74fc1c557
Federica Santoro <federica.santoro.1993@gmail.com>
2021-11-21 18:39:58 +0100

    up

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ document.addEventListener('DOMContentLoaded', () =>{
             //console.log(event.target.classList[0]); // L'event.target mi indica quale pulsante è stato premuto
             switch(direction){
                 case 'up':
+                    
+                    const interval1 = setInterval(() => {
+                        goldfish.classList.toggle("swim__up");
+                    }, 400);
+                    
+                    setTimeout(() => {
+                        clearInterval(interval1);
+                    }, 2500);
+
+                    goldfish.classList.add("up");
+                                  
                     goldfish.style.top = '100px';
-                    const death = setTimeout(() => {goldfish.style.transform = 'rotate(180deg)'},3000);
+                    const death = setTimeout(() => {goldfish.style.transform = 'rotate(-90deg)'},3000);
                     break;
                 case 'left':
                     goldfish.style.left = '40px';
@@ -22,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () =>{
                     break;
                 case 'right':
                     const interval = setInterval(() => {
-                        goldfish.classList.toggle("swim");
+                        goldfish.classList.toggle("swim__right");
                     }, 400);
                     
                     setTimeout(() => {
